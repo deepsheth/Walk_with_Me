1f5a108d7cd7fdecf55d15cdfb25eaad77bc46bf
Deep Sheth <contact@deepsheth.com>
2016-12-03 05:00:22 -0500

    Geofire works with markers on map

--- public/js/map_script.js
+++ public/js/map_script.js
@@ -1,39 +1,4 @@
-function postWalk(user_id,start_lat,start_lon,end_lat,end_lon,destination_name){
-	var firebaseRef = firebase.database().ref("posted_walks");
-	var geoFire = new GeoFire(firebaseRef);
-
-	var data = {}
-	data["poster_id"] = user_id;
-	data["start_lat"] = start_lat;
-	data["start_lon"] = start_lon;
-	data["end_lat"] = end_lat;
-	data["end_lon"] = end_lon;
-	data["destination_name"] = destination_name;
-	data["time"] = new Date().getTime();
-
-	var walk_id = firebaseRef.push().getKey();
-	firebaseRef.child("posted_walks").child(walk_id).setValue(data);
-
-	geoFire.setLocation(walk_id, new GeoLocation(start_lat,start_lon));
-	return true;
-}
-
-function get_posted_walks(latitude, longitude){
-	var firebaseRef = firebase.database().ref("posted_walks");
-	var geoFire = new GeoFire(firebaseRef);
-
-	center = {};
-	center["latitude"] = latitude;
-	center["longitude"] = longitude;
-
-	var geoQuery = geoFire.query({
-		center: center,
-		radius: 2 //kilometers
-	});
-
-	// lol idk what to do with geoQuery
-}
-
+var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: 40.6069, lng: -75.3783},
@@ -51,6 +16,8 @@ function initMap() {
         addMarker(map, pos, "current_location");
 
 	    map.setCenter(pos);
+	    get_posted_walks(pos.lat, pos.lng);
+
 	  }, function() {
 	    handleLocationError(true, infoWindow, map.getCenter());
 	  });
@@ -58,7 +25,6 @@ function initMap() {
 	  // Browser doesn't support Geolocation
 	  handleLocationError(false, infoWindow, map.getCenter());
 	}
-
 }
 
 	function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -81,7 +47,61 @@ function addMarker (map, pos, icon) {
 		map: map,
 		icon: marker_icon
     });
+}
 
+function addMarkerHelper(key, location, distance, icon) {
+	var pos = {
+	    	lat: location[0],
+	    	lng: location[1]
+	}
+	
+	console.log(pos);
+	addMarker(map, pos, "red");
+}
+
+function postWalk(user_id,start_lat,start_lon,end_lat,end_lon,destination_name){
+	var firebaseRef = firebase.database().ref();
+	var geoFire = new GeoFire(firebaseRef.child('posted_walks_geofire'));
+
+	var data = {}
+	data["poster_id"] = user_id;
+	data["start_lat"] = start_lat;
+	data["start_lon"] = start_lon;
+	data["end_lat"] = end_lat;
+	data["end_lon"] = end_lon;
+	data["destination_name"] = destination_name;
+	data["time"] = new Date().getTime();
+
+	var walk_id = firebaseRef.child('posted_walks').push().getKey();
+	firebaseRef.child("posted_walks").child(walk_id).set(data);
+
+	geoFire.set(walk_id, [start_lat,start_lon]);
+	return true;
+}
+
+function get_posted_walks(latitude, longitude){
+	console.log("getting walks from " + latitude + " , " + longitude);
+
+	var firebaseRef = firebase.database().ref("posted_walks_geofire");
+	var geoFire = new GeoFire(firebaseRef);
+
+	center = [latitude,longitude];
+	//center["latitude"] = latitude;
+	//center["longitude"] = longitude;
+
+	var geoQuery = geoFire.query({
+		center: center,
+		radius: 2 //kilometers
+	});
+
+	var onKeyEnteredRegistration = geoQuery.on("key_entered", function(key, location, distance) {
+	    
+		addMarkerHelper(key, location, distance, "red");
+
+		// all_markers.push(marker);
+		// console.log("Key: " + key + " Location: " + location + " Distance: " + distance);
+	});
+	// lol idk what to do with geoQuery
 }
 
 function sign_out() {
