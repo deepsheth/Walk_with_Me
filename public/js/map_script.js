082983b6ec2363c6b2ea71e7ce954a6011f182f7
Deep Sheth <contact@deepsheth.com>
2016-12-03 09:31:44 -0500

    ore problems with the home page

--- public/js/map_script.js
+++ public/js/map_script.js
@@ -1,23 +1,30 @@
-function postWalk(user_id,start_lat,start_lon,end_lat,end_lon,destination_name){
-	var firebaseRef = firebase.database().ref("posted_walks");
-	var geoFire = new GeoFire(firebaseRef);
+function postWalk(user_id, user_name, num_walks, start_lat,start_lon,end_lat,end_lon,destination_name, icon, usr_name){
 
-	var data = {}
+	var firebaseRef = firebase.database().ref();
+	var geoFire = new GeoFire(firebaseRef.child('posted_walks_geofire_test'));
+
+	var data = {};
 	data["poster_id"] = user_id;
-	data["start_lat"] = start_lat;
-	data["start_lon"] = start_lon;
-	data["end_lat"] = end_lat;
-	data["end_lon"] = end_lon;
+	data["poster_name"] = user_name;
+	data["poster_num_walks"] = num_walks;
+	data["start_latitude"] = start_lat;
+	data["start_longitude"] = start_lon;
+	data["end_latitude"] = end_lat;
+	data["end_longitude"] = end_lon;
 	data["destination_name"] = destination_name;
+	data["icon"] = false;
 	data["time"] = new Date().getTime();
 
-	var walk_id = firebaseRef.push().getKey();
-	firebaseRef.child("posted_walks").child(walk_id).setValue(data);
+	var walk_id = firebaseRef.child('posted_walks_test').push().getKey();
+	firebaseRef.child("posted_walks_test").child(walk_id).set(data);
 
-	geoFire.setLocation(walk_id, new GeoLocation(start_lat,start_lon));
+	geoFire.set(walk_id, [start_lat,start_lon]);
 	return true;
+
 }
 
+/*
+	old
 function get_posted_walks(latitude, longitude){
 	var firebaseRef = firebase.database().ref("posted_walks");
 	var geoFire = new GeoFire(firebaseRef);
@@ -35,7 +42,7 @@ function get_posted_walks(latitude, longitude){
 	  console.log(key + " entered query at " + location + " (" + distance + " km from center)");
 	});
 	// lol idk what to do with geoQuery
-}
+}*/
 
 var map;
 function initMap() {
@@ -88,27 +95,44 @@ function addMarker (map, pos, icon, key) {
 		return;	
 	}
 
- 	var angled_marker = {
-          path: 'm12 0c-4.4183 2.3685e-15 -8 3.5817-8 8 0 1.421 0.3816 2.75 1.0312 3.906 0.1079 0.192 0.221 0.381 0.3438 0.563l6.625 11.531 6.625-11.531c0.102-0.151 0.19-0.311 0.281-0.469l0.063-0.094c0.649-1.156 1.031-2.485 1.031-3.906 0-4.4183-3.582-8-8-8zm0 4c2.209 0 4 1.7909 4 4 0 2.209-1.791 4-4 4-2.2091 0-4-1.791-4-4 0-2.2091 1.7909-4 4-4z',
-          scale: 1.75,
-          fillColor: '#e74c3c',
-          fillOpacity: 1,
-          strokeColor: '#c0392b',
-          strokeWeight: 3,
-          rotation: 45,
-    };
+	var walkData = {};
+	firebase.database().ref('/posted_walks_test/' + key).once('value').then(function(snapshot) {
+		console.log(snapshot.val());
+		walkData['poster_id'] = snapshot.val().poster_id;
+		walkData['start_latitude'] = snapshot.val().start_latitude;
+		walkData['start_longitude'] = snapshot.val().start_longitude;
+		walkData['end_latitude'] = snapshot.val().end_latitude;
+		walkData['end_longitude'] = snapshot.val().end_longitude;
+		walkData['destination_name'] = snapshot.val().destination_name;
+		walkData['time'] = snapshot.val().time;
 
-    var title;
-    var map_marker = new google.maps.Marker({
-    	position: pos,
-		icon: angled_marker,
-		map: map
-	});
+		var angle = getAngle(walk_data["start_latitude"], walk_data["start_longitude"], walk_data["end_latitude"], walk_data["end_longitude"]);
+		console.log("angle: " + angle);
 
-	map_marker.addListener('click', function() {
-		var walk_info = get_walk_data(key, "posted");
-		display_walk_info(walk_info);
-    });
+	 	var angled_marker = {
+	          path: 'm12 0c-4.4183 2.3685e-15 -8 3.5817-8 8 0 1.421 0.3816 2.75 1.0312 3.906 0.1079 0.192 0.221 0.381 0.3438 0.563l6.625 11.531 6.625-11.531c0.102-0.151 0.19-0.311 0.281-0.469l0.063-0.094c0.649-1.156 1.031-2.485 1.031-3.906 0-4.4183-3.582-8-8-8zm0 4c2.209 0 4 1.7909 4 4 0 2.209-1.791 4-4 4-2.2091 0-4-1.791-4-4 0-2.2091 1.7909-4 4-4z',
+	          scale: 1.75,
+	          fillColor: '#e74c3c',
+	          fillOpacity: 1,
+	          strokeColor: '#c0392b',
+	          strokeWeight: 3,
+	          rotation: angle,
+	    };
+
+	    var title;
+	    var map_marker = new google.maps.Marker({
+	    	position: pos,
+			icon: angled_marker,
+			map: map
+		});
+
+		map_marker.addListener('click', function() {
+			var walk_info = get_walk_data(key, "posted");
+			display_walk_info(walk_info);
+	    });
+		});
+
+	
 
 
 }
@@ -122,6 +146,34 @@ function addMarkerHelper(key, location, distance, icon) {
 	addMarker(map, pos, "red", key);
 }
 
+function radians(n) {
+  return n * (Math.PI / 180);
+}
+function degrees(n) {
+  return n * (180 / Math.PI);
+}
+
+function getAngle(start_lat, start_lng, end_lat, end_lng){
+  startLat = radians(start_lat);
+  startLong = radians(start_lng);
+  endLat = radians(end_lat);
+  endLong = radians(end_lng);
+
+  var dLong = endLong - startLong;
+
+  var dPhi = Math.log(Math.tan(endLat/2.0+Math.PI/4.0)/Math.tan(startLat/2.0+Math.PI/4.0));
+  if (Math.abs(dLong) > Math.PI){
+    if (dLong > 0.0)
+       dLong = -(2.0 * Math.PI - dLong);
+    else
+       dLong = (2.0 * Math.PI + dLong);
+  }
+
+  return (degrees(Math.atan2(dLong, dPhi)) + 360.0) % 360.0;
+}
+
+/*
+	More accurate
 function postWalk(user_id,start_lat,start_lon,end_lat,end_lon,destination_name){
 	var firebaseRef = firebase.database().ref();
 	var geoFire = new GeoFire(firebaseRef.child('posted_walks_geofire'));
@@ -140,12 +192,12 @@ function postWalk(user_id,start_lat,start_lon,end_lat,end_lon,destination_name){
 
 	geoFire.set(walk_id, [start_lat,start_lon]);
 	return true;
-}
+}*/
 
 function get_posted_walks(latitude, longitude){
 
-	var firebaseRef = firebase.database().ref("posted_walks_geofire");
-	var geoFire = new GeoFire(firebaseRef);
+	var firebaseRef = firebase.database().ref();
+	var geoFire = new GeoFire(firebaseRef.child('posted_walks_geofire_test'));
 
 	center = [latitude,longitude];
 	//center["latitude"] = latitude;
@@ -161,7 +213,7 @@ function get_posted_walks(latitude, longitude){
 		addMarkerHelper(key, location, distance, "red");
 
 		// all_markers.push(marker);
-		// console.log("Key: " + key + " Location: " + location + " Distance: " + distance);
+		console.log("Key: " + key + " Location: " + location + " Distance: " + distance);
 	});
 	// lol idk what to do with geoQuery
 }
@@ -234,7 +286,9 @@ function get_walk_data(walkID, type){
 function display_walk_info(walk_info) {
 	$('#walk-info-modal').modal('open');
 
-	
+	$('.dyn_location').val(walk_info["destination_name"]);
+	var dyn_src = "https://maps.googleapis.com/maps/api/staticmap?center="+walk_info["start_latitude"] + "," + walk_info["start_longitude"] + "&zoom=15&size=450x300&markers=color:0x2196F3%7Clabel:S%7C"+walk_info["end_latitude"] + "," + walk_info["end_longitude"] + "&markers=color:0xE74C3C%7Clabel:D%7C40.60506,-75.387174&key=AIzaSyBn3v_1yzu_bEKpxBUCPKtQVJngCYoamgE";
+	$('.dyn_map').attr("src", dyn_src);
 }
 
 
