add4f949bae172c3208d2389b3ea499de3251e3e
Deep Sheth <contact@deepsheth.com>
2016-12-03 10:44:28 -0500

    created walk info page

---
+++ public/js/script_details.js
@@ -0,0 +1,23 @@
+function initMiniMap() {
+	map = new google.maps.Map(document.getElementById('mini-map'), {
+	  center: {lat: 40.6069, lng: -75.3783},
+	  zoom: 17
+	});
+
+	// Try HTML5 geolocation.
+	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(function(position) {
+	    var pos = {
+	      lat: position.coords.latitude,
+	      lng: position.coords.longitude
+	    };
+
+	    map.setCenter(pos);
+	  }, function() {
+	    handleLocationError(true, infoWindow, map.getCenter());
+	  });
+	} else {
+	  // Browser doesn't support Geolocation
+	  handleLocationError(false, infoWindow, map.getCenter());
+	}
+}
