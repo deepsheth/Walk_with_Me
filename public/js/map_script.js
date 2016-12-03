d66f1499deed834689568f36135b92a6dce1d0c2
Deep Sheth <contact@deepsheth.com>
2016-12-03 01:15:26 -0500

    Auth works, better aligned header, account creation works

--- public/js/map_script.js
+++ public/js/map_script.js
@@ -1,3 +1,39 @@
+function postWalk(user_id,start_lat,start_lon,end_lat,end_lon,destination_name){
+	var firebaseRef = firebase.database().ref("posted_walks");
+	var geoFire = new GeoFire(firebaseRef);
+
+	var data = {}
+	data["poster_id"] = user_id;
+	data["start_lat"] = start_lat;
+	data["start_lon"] = start_lon;
+	data["end_lat"] = end_lat;
+	data["end_lon"] = end_lon;
+	data["destination_name"] = destination_name;
+	data["time"] = new Date().getTime();
+
+	var walk_id = firebaseRef.push().getKey();
+	firebaseRef.child("posted_walks").child(walk_id).setValue(data);
+
+	geoFire.setLocation(walk_id, new GeoLocation(start_lat,start_lon));
+	return true;
+}
+
+function get_posted_walks(latitude, longitude){
+	var firebaseRef = firebase.database().ref("posted_walks");
+	var geoFire = new GeoFire(firebaseRef);
+
+	center = {};
+	center["latitude"] = latitude;
+	center["longitude"] = longitude;
+
+	var geoQuery = geoFire.query({
+		center: center,
+		radius: 2 //kilometers
+	});
+
+	// lol idk what to do with geoQuery
+}
+
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: 40.6069, lng: -75.3783},
@@ -12,6 +48,15 @@ function initMap() {
 	      lng: position.coords.longitude
 	    };
 
+	    var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+        var beachMarker = new google.maps.Marker({
+          position: {lat: -33.890, lng: 151.274},
+          map: map,
+          icon: image
+        });
+
+        addMarker(map, pos, icon);
+
 	    map.setCenter(pos);
 	  }, function() {
 	    handleLocationError(true, infoWindow, map.getCenter());
@@ -25,6 +70,15 @@ function initMap() {
 	function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 		Materialize.toast("Geolocation failed.");
 	}
+
+function addMarker (map, pos, icon) {
+	var beachMarker = new google.maps.Marker({
+		position: {lat: -33.890, lng: 151.274},
+		map: map,
+		icon: image
+    });
+
+}
 
 function sign_out() {
 	firebase.auth().signOut().then(function() {
--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,3 @@
-function postWalk(user_id,start_lat,start_lon,end_lat,end_lon,destination_name){
-	var firebaseRef = firebase.database().ref("posted_walks");
-	var geoFire = new GeoFire(firebaseRef);
-
-	var data = {}
-	data["poster_id"] = user_id;
-	data["start_lat"] = start_lat;
-	data["start_lon"] = start_lon;
-	data["end_lat"] = end_lat;
-	data["end_lon"] = end_lon;
-	data["destination_name"] = destination_name;
-	data["time"] = new Date().getTime();
-
-	var walk_id = firebaseRef.push().getKey();
-	firebaseRef.child("posted_walks").child(walk_id).setValue(data);
-
-	geoFire.setLocation(walk_id, new GeoLocation(start_lat,start_lon));
-	return true;
-}
-
-function get_posted_walks(latitude, longitude){
-	var firebaseRef = firebase.database().ref("posted_walks");
-	var geoFire = new GeoFire(firebaseRef);
-
-	center = {};
-	center["latitude"] = latitude;
-	center["longitude"] = longitude;
-
-	var geoQuery = geoFire.query({
-		center: center,
-		radius: 2 //kilometers
-	});
-
-	// lol idk what to do with geoQuery
-}
-
 function log_in(email, password) {
 
 
@@ -57,7 +21,7 @@ function sign_out() {
 	});	
 }
 
-function create_user(email, password, name, school) {
+function create_user(email, password, name) {
 
 	var initUserData = {
 			name: name,
@@ -124,7 +88,7 @@ $(document).ready( function() {
 
 		// To avoid premature redirection
 		stop_authStateListener();
-		create_user($('#create_username').val(), $('#create_password').val(), $('#create_name').val(), $('#create_school').val());
+		create_user($('#create_username').val(), $('#create_password').val(), $('#create_name').val());
 	});
 
 	
